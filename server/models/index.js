950b83c608eedbb38455c4344060ef440e0dc53a
David Señas Sanvicente <davsensan@gmail.com>
2017-04-05 11:11:28 +0200

    Task: #9317, #9318. Creado modelo Organization y relaciones con el modelo usuario

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,11 @@
 module.exports = function (sequelize){
 	//Importamos todos los modelos, hacemos las relaciones entre ellos y los devolvemos
 	const User = require('./user')(sequelize);
+	const Organization = require('./Organization')(sequelize);
+	User.belongsTo(Organization);
+	Organization.hasMany(User);
 	return {
-		User: User
+		User: User,
+		Organization: Organization
 	};
 };
