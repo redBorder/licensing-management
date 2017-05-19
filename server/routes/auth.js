5a1ead0eb4eb545b41352b595ec9ae9f0574ac2a
David Señas Sanvicente <davsensan@gmail.com>
2017-05-19 16:42:59 +0200

    Modificado http, para utilizar el enlace para https

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -131,7 +131,7 @@ router.post('/forgot', function(req, res, next) {
         subject: 'Node.js Password Reset',
         text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
           'Please click on the following link before one hour, or paste this into your browser to complete the process:\n\n' +
-          'http://' + req.headers.host + '/#/reset/' + token + '\n\n' +
+          'https://' + req.headers.host + '/#/reset/' + token + '\n\n' +
           'If you did not request this, please ignore this email and your password will remain unchanged.\n'
       };
       smtpTransport.sendMail(mailOptions,function(err) {
